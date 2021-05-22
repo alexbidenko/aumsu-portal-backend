19fdd76fc804252700a41f3cf81dc6c8bff492e6
Alexander <alexbidenko1998@gmail.com>
2021-05-22 12:52:27 +0300

    Fix required fields

--- src/aumsu/entities/student.entity.go
+++ src/aumsu/entities/student.entity.go
@@ -5,8 +5,8 @@ type Student struct {
 	Login string `json:"login" valid:"required,type(string),length(5|255)" gorm:"size:255"`
 	Password string `json:"password" valid:"required,type(string),length(8|255)" gorm:"size:255"`
 	Token string `json:"token" valid:"required,type(string),length(5|255)" gorm:"size:255"`
-	FirstName string `json:"firstName" valid:"type(string),length(2|255)" gorm:"size:255"`
-	LastName string `json:"lastName" valid:"type(string),length(2|255)" gorm:"size:255"`
+	FirstName string `json:"firstName" valid:"required,type(string),length(2|255)" gorm:"size:255"`
+	LastName string `json:"lastName" valid:"required,type(string),length(2|255)" gorm:"size:255"`
 	Avatar string `json:"avatar" valid:"type(string),length(1|255)" gorm:"size:255"`
 	Status string `json:"status" valid:"type(string),length(1|255)" gorm:"size:255"`
 	Patronymic string `json:"patronymic" valid:"type(string),length(2|255),optional" gorm:"size:255"`
