064960b54d8b49f425b106aed23d491633a997f2
Alexander Bidenko <alexbidenko1998@gmail.com>
2021-11-24 22:42:56 +0300

    Fix

--- modules/message.module.go
+++ modules/message.module.go
@@ -26,14 +26,14 @@ func (messageModel MessageModel) Create(model *entities.Message) {
 
 func (messageModel MessageModel) All() []entities.Message {
 	var messages []entities.Message
-	dif.DB.Model(&entities.Message{}).Limit(40).Order("created_at desc").Find(&messages)
+	dif.DB.Model(&entities.Message{}).Limit(40).Order("created_at desc").Order("id asc").Find(&messages)
 	return messages
 }
 
 func (messageModel MessageModel) GetById(id string) (entities.Message, error) {
 	var message entities.Message
 	err := dif.DB.Model(&entities.Message{}).Preload("Comments", func(db *gorm.DB) *gorm.DB {
-		return db.Order("comment.created_at ASC")
+		return db.Order("comments.created_at ASC").Order("comments.id ASC")
 	}).Preload("Comments.User").First(&message, id).Error
 
 	if err != nil {
