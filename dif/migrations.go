661fa58e72c5a60e042eb54639bfee1915f353b5
Alexander Bidenko <alexbidenko1998@gmail.com>
2021-11-25 15:26:18 +0300

    Fix

--- dif/migrations.go
+++ dif/migrations.go
@@ -2,25 +2,8 @@ package dif
 
 import (
 	"aumsu.portal.backend/entities"
-	models "aumsu.portal.backend/modules"
-	"golang.org/x/crypto/bcrypt"
 )
 
 func Migrate() {
 	DB.AutoMigrate(&entities.Student{}, &entities.Message{}, &entities.Comment{})
-	
-	var student entities.Student
-	DB.Model(&entities.Student{}).Where("login = ?", "alexbidenko").First(&student)
-	if student.Password == "12345678" {
-		var students []entities.Student
-		DB.Model(&entities.Student{}).Find(&students)
-
-		for _, item := range students {
-			bytes, _ := bcrypt.GenerateFromPassword([]byte(item.Password), 14)
-			item.Password = string(bytes)
-
-			var studentModule models.StudentModel
-			studentModule.Update(item.Id, &item)
-		}
-	}
 }
--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"aumsu.portal.backend/controllers"
 	"aumsu.portal.backend/dif"
+	"aumsu.portal.backend/entities"
+	models "aumsu.portal.backend/modules"
 	_ "database/sql"
 	"fmt"
 	"github.com/gorilla/mux"
+	"golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
 )
@@ -17,6 +20,21 @@ func main() {
 	}
 
 	dif.Migrate()
+
+	var student entities.Student
+	dif.DB.Model(&entities.Student{}).Where("login = ?", "alexbidenko").First(&student)
+	if student.Password == "12345678" {
+		var students []entities.Student
+		dif.DB.Model(&entities.Student{}).Find(&students)
+
+		for _, item := range students {
+			bytes, _ := bcrypt.GenerateFromPassword([]byte(item.Password), 14)
+			item.Password = string(bytes)
+
+			var studentModule models.StudentModel
+			studentModule.Update(item.Id, &item)
+		}
+	}
 
 	r := mux.NewRouter()
 
