312eab89bc7e179344bee57b5a8e3cefbb4e6098
Alexander Bidenko <alexbidenko1998@gmail.com>
2021-12-26 22:12:02 +0300

    Try parse schedule content

--- modules/study-group.module.go
+++ modules/study-group.module.go
@@ -3,6 +3,8 @@ package models
 import (
 	"aumsu.portal.backend/dif"
 	"aumsu.portal.backend/entities"
+	"encoding/json"
+	"fmt"
 )
 
 type StudyGroupModel struct {
@@ -14,8 +16,18 @@ func (StudyGroupModel) All() []entities.StudyGroup {
 	return studyGroups
 }
 
-func (StudyGroupModel) GetSchedule(studyGroupId string) (entities.StudyGroupSchedule, error) {
-	var studyGroupSchedule entities.StudyGroupSchedule
+func (StudyGroupModel) GetSchedule(studyGroupId string) (map[string]interface{}, error) {
+	var studyGroupSchedule map[string]interface{}
 	err := dif.DB.Model(&entities.StudyGroupSchedule{}).Where("study_group_id", studyGroupId).First(&studyGroupSchedule).Error
+
+	if err == nil {
+		var data interface{}
+		err = json.Unmarshal([]byte(fmt.Sprintf("%v", studyGroupSchedule["content"])), &data)
+
+		if err == nil {
+			studyGroupSchedule["content"] = data
+		}
+	}
+
 	return studyGroupSchedule, err
 }
